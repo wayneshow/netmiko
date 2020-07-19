df7a06a417a1c5872426e82a5238111ee937072a
Kirk Byers <ktbyers@twb-tech.com>
2020-07-18 19:06:47 -0700

    Test suite

--- tests/test_suite_alt.sh
+++ tests/test_suite_alt.sh
@@ -57,10 +57,10 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_config_acl.py --test_device arista_sw \
 \
 && echo "Juniper" \
-&& py.test -v test_netmiko_scp.py --test_device juniper_srx \
-&& py.test -v test_netmiko_show.py --test_device juniper_srx \
-&& py.test -v test_netmiko_config.py --test_device juniper_srx \
-&& py.test -v test_netmiko_commit.py --test_device juniper_srx \
+&& py.test -x -v test_netmiko_scp.py --test_device juniper_srx \
+&& py.test -x -v test_netmiko_show.py --test_device juniper_srx \
+&& py.test -x -v test_netmiko_config.py --test_device juniper_srx \
+&& py.test -x -v test_netmiko_commit.py --test_device juniper_srx \
 \
 && echo "Cisco ASA" \
 && py.test -v test_netmiko_show.py --test_device cisco_asa \
@@ -73,11 +73,6 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_show.py --test_device cisco_xrv \
 && py.test -v test_netmiko_config.py --test_device cisco_xrv \
 && py.test -v test_netmiko_commit.py --test_device cisco_xrv \
-\
-&& echo "Cisco IOS-XR (Azure)" \
-&& py.test -v test_netmiko_show.py --test_device cisco_xr_azure \
-&& py.test -v test_netmiko_config.py --test_device cisco_xr_azure \
-&& py.test -v test_netmiko_commit.py --test_device cisco_xr_azure \
 \
 && echo "Cisco NXOS" \
 && py.test -v test_netmiko_scp.py --test_device nxos1 \
@@ -112,3 +107,8 @@ exit $RETURN_CODE
 # && py.test -v test_netmiko_scp.py --test_device cisco881_key \
 # && py.test -v test_netmiko_scp.py --test_device cisco881 \
 # && py.test -v test_netmiko_scp.py --test_device cisco881_fast \
+# #&& echo "Cisco IOS-XR (Azure)" \
+# #&& py.test -v test_netmiko_show.py --test_device cisco_xr_azure \
+# #&& py.test -v test_netmiko_config.py --test_device cisco_xr_azure \
+# #&& py.test -v test_netmiko_commit.py --test_device cisco_xr_azure \
+# #\
